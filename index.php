ec889c92e5270f8891b9a571fcb0e1156aa01483
Daniel Silva Ferreira <danielsilva@usp.br>
2023-09-27 18:08:35 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+//treino-svg/index.php
+define('INDEX_FILE_LOCATION', __FILE__);
+$application = require 'svg.html';
---
+++ svg.php
@@ -0,0 +1,20 @@
+<?php
+
+$circulo01 = 01;
+$circulo02 = 02;
+$circulo03 = 03;
+$circulo04 = 04;
+
+?>
+
+<!-- Conteúdo do Círculo 01 -->
+<text x="755" y="100" fill="black"><?= $circulo01; ?></text>
+
+<!-- Conteúdo do Círculo 02 -->
+<text x="260" y="80" fill="black"><?= $circulo02; ?></text>
+
+<!-- Conteúdo do Círculo 03 -->
+<text x="60" y="280" fill="black"><?= $circulo03; ?></text>
+
+<!-- Conteúdo do Círculo 04 -->
+<text x="260" y="280" fill="black"><?= $circulo04; ?></text>
